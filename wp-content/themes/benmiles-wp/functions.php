190fcd7302625d17685dddad4fef7f3b72491b3c
Ben Miles <bcgm3studios@gmail.com>
2022-04-22 00:19:31 -0400

    Disable Gutenberg/Blocks CSS

--- wp-content/themes/benmiles-wp/functions.php
+++ wp-content/themes/benmiles-wp/functions.php
@@ -8,4 +8,12 @@ wp_enqueue_style( 'animate', get_template_directory_uri() . '/assets/css/animate
 wp_enqueue_style( 'ie10-viewport-bug-workaround', get_template_directory_uri() . '/assets/css/ie10-viewport-bug-workaround.css',false,'','all');
 wp_enqueue_style( 'custom', get_template_directory_uri() . '/assets/css/custom.css',false,'','all');
 
+//Remove Gutenberg Block Library CSS from loading on the frontend
+function smartwp_remove_wp_block_library_css(){
+    wp_dequeue_style( 'wp-block-library' );
+    wp_dequeue_style( 'wp-block-library-theme' );
+    wp_dequeue_style( 'wc-blocks-style' ); // Remove WooCommerce block CSS
+} 
+add_action( 'wp_enqueue_scripts', 'smartwp_remove_wp_block_library_css', 100 );
+
 ?>
