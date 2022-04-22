3d14c15d8a1f3096b030a74c70938b2281dc1d06
Ben Miles <bcgm3studios@gmail.com>
2022-04-22 02:35:40 -0400

    Move Modal to Footer

--- wp-content/themes/benmiles-wp/footer.php
+++ wp-content/themes/benmiles-wp/footer.php
@@ -43,11 +43,44 @@
         </footer>
         <!-- footer -->
 
+		<!-- back to top -->
         <button class="btn btn-default scroll" id="backToTop" data-section="hey">
             <i class="fa fa-angle-up" aria-hidden="true"></i>
         </button>
 
         <script>var jsonPortfolio = <?php echo $jsonPortfolio ?>;</script>
+		<!-- modal -->
+		<div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+			<div class="modal-dialog" role="document">
+				<div class="modal-content">
+					<div class="modal-body">
+						<div class="row">
+							<div class="image col-md-8">
+								<button type="button" class="close d-md-none" data-dismiss="modal" aria-label="Close">
+									<span aria-hidden="true">&times;</span>
+								</button>
+								<div class="wrapper"></div>
+							</div>
+							<div class="info col-md-4">
+								<button type="button" class="close d-none d-md-block" data-dismiss="modal" aria-label="Close">
+									<span aria-hidden="true">&times;</span>
+								</button>
+								<div class="row">
+									<div class="col-12">
+										<div class="header"></div>
+										<div class="meta"></div>
+										<div class="thumbs"></div>
+										<div class="body"></div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- / modal -->
+
 		<?php wp_footer(); ?>
 
     </body>
--- wp-content/themes/benmiles-wp/page-home.php
+++ wp-content/themes/benmiles-wp/page-home.php
@@ -100,9 +100,7 @@ get_header();
 
             <!-- gallery -->
             <div class="row animated" id="gallery" data-animation="fadeIn" style="animation-delay: 0.4s;">
-                <?php echo 'hello';
-                echo 'bye';
-				buildPortfolio( $arrayPortfolio ); ?>
+                <?php buildPortfolio( $arrayPortfolio ); ?>
             </div>
             <!-- / gallery -->
 
@@ -340,36 +338,5 @@ get_header();
 </section>
 <!-- / about -->
 
-<!-- modal -->
-<div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-body">
-                <div class="row">
-                    <div class="image col-md-8">
-                        <button type="button" class="close d-md-none" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                        <div class="wrapper"></div>
-                    </div>
-                    <div class="info col-md-4">
-                        <button type="button" class="close d-none d-md-block" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                        <div class="row">
-                            <div class="col-12">
-                                <div class="header"></div>
-                                <div class="meta"></div>
-                                <div class="thumbs"></div>
-                                <div class="body"></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- / modal -->
 
 <?php get_footer(); ?>
