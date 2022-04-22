9aa05f5bdc4f52f053a58781792483493bd3c3ca
Ben Miles <bcgm3studios@gmail.com>
2022-04-22 02:13:56 -0400

    Disable More Default CSS

--- wp-content/themes/benmiles-wp/functions.php
+++ wp-content/themes/benmiles-wp/functions.php
@@ -4,7 +4,7 @@
 function smartwp_remove_wp_block_library_css(){
 	wp_dequeue_style( 'wp-block-library' );
 	wp_dequeue_style( 'wp-block-library-theme' );
-	wp_dequeue_style( 'wc-blocks-style' ); // Remove WooCommerce block CSS
+	wp_dequeue_style( 'wc-blocks-style' );
 } 
 add_action( 'wp_enqueue_scripts', 'smartwp_remove_wp_block_library_css', 100 );
 
