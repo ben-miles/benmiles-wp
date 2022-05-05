6eb3db7bf2808b0ae1d18ba9f47f16910192085a
Ben Miles <bcgm3studios@gmail.com>
2022-05-04 21:30:38 -0400

    Add Archive Template for Portfolio Items

---
+++ wp-content/themes/benmiles-wp/archive-portfolio.php
@@ -0,0 +1,46 @@
+<?php 
+/*
+Template Name: Archive Portfolio
+*/
+// error_reporting(0);
+get_header(); 
+?>
+
+<!-- archive-portfolio -->
+<section id="archive-portfolio">
+
+    <!-- header -->
+    <div class="section-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-7 col-xl-8">
+                    <h2 class="animated" data-animation="fadeInUp">Archive Portfolio</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- / header -->
+
+    <!-- content -->
+    <div class="section-body">
+        <div class="container">
+
+            <div class="row">
+                <div class="col-sm-7 col-xl-8">
+                    <div class="animated card archive-portfolio" style="animation-delay: .1s;" data-animation="fadeInUp">
+                        <div class="card-block">
+                            <p class="card-text">Page not found.</p>
+                            <p class="card-text">Go to home page? Search?</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <!-- / content -->
+
+</section>
+<!-- / about -->
+
+<?php get_footer(); ?>
