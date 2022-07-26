71f857fd799494c160e0f2e4c4c120743346b07a
Ben Miles <bcgm3studios@gmail.com>
2022-07-26 10:09:46 -0400

    New Footer

--- wp-content/themes/benmiles-wp/footer.php
+++ wp-content/themes/benmiles-wp/footer.php
@@ -1,55 +1,42 @@
 	    <!-- footer -->
         <footer id="footer">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-7 col-xl-8">
+            <div class="container-new">
+                <div class="row-new">
+                    <div class="column column-1">
 
-                        <ul class="social">
-                            <li class="animated" data-animation="fadeIn">
-                                <a class="facebook" href="https://www.facebook.com/bcgm3/" target="_blank">
-                                    <i class="fa fa-facebook" data-animation="fadeInUp"></i>
-                                </a>
-                            </li>
-                            <li class="animated" data-animation="fadeIn" style="animation-delay: .1s;">
-                                <a class="twitter" href="https://twitter.com/bcgm3" target="_blank">
-                                    <i class="fa fa-twitter"></i>
-                                </a>
-                            </li>
-                            <li class="animated" data-animation="fadeIn" style="animation-delay: .2s;">
-                                <a class="instagram" href="https://www.instagram.com/bencgmiles/" target="_blank">
-                                    <i class="fa fa-instagram"></i>
-                                </a>
-                            </li>
-                            <li class="animated" data-animation="fadeIn" style="animation-delay: .3s;">
-                                <a class="linkedin" href="https://www.linkedin.com/in/benjaminmiles/" target="_blank">
-                                    <i class="fa fa-linkedin"></i>
-                                </a>
-                            </li>
-                            <li class="animated" data-animation="fadeIn" style="animation-delay: .4s;">
-                                <a class="codepen" href="https://codepen.io/benmiles/" target="_blank">
-                                    <i class="fa fa-codepen"></i>
-                                </a>
-                            </li>
-                            <li class="animated" data-animation="fadeIn" style="animation-delay: .5s;">
-                                <a class="github" href="https://github.com/ben-miles" target="_blank">
-                                    <i class="fa fa-github"></i>
-                                </a>
-                            </li>
-                        </ul>
-
+						<!-- social media links -->
+						<ul class="social-media">
+							<li>
+								<a href="https://www.linkedin.com/in/benjaminmiles/" target="_blank">
+									<?php displaySVG( 'LinkedIn' ); ?>
+								</a>
+							</li>
+							<li>
+								<a href="https://codepen.io/benmiles/" target="_blank">
+									<?php displaySVG( 'CodePen' ); ?>
+								</a>
+							</li>
+							<li>
+								<a href="https://github.com/ben-miles" target="_blank">
+									<?php displaySVG( 'GitHub' ); ?>
+								</a>
+							</li>
+						</ul>
+						<!-- / social media links -->
+						
                     </div>
                 </div>
             </div>
         </footer>
-        <!-- footer -->
+        <!-- / footer -->
 
 		<!-- back to top -->
-        <button class="btn btn-default scroll" id="backToTop" data-section="hey">
+        <button class="btn btn-default scroll" id="backToTop" data-section="intro">
             <i class="fa fa-angle-up" aria-hidden="true"></i>
         </button>
 
 		<!-- modal -->
-		<div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+		<!-- <div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
 			<div class="modal-dialog" role="document">
 				<div class="modal-content">
 					<div class="modal-body">
@@ -77,7 +64,7 @@
 					</div>
 				</div>
 			</div>
-		</div>
+		</div> -->
 		<!-- / modal -->
 
 		<?php wp_footer(); ?>
