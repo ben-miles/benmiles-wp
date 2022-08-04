3c0da182a88c79b70ca0218fd9106bd532952a2f
Ben Miles <bcgm3studios@gmail.com>
2022-08-04 12:29:53 -0400

    Add Single-Portfolio-Item Page-Template

--- wp-content/themes/benmiles-wp/single-portfolio.php
+++ wp-content/themes/benmiles-wp/single-portfolio.php
@@ -8,71 +8,110 @@ get_header();
 
 <!-- single-portfolio -->
 <section id="single-portfolio">
-	
-	<?php 
-		// Start the loop.
-		while(have_posts()) : the_post(); 
-	?>
+    <div class="container-new">
+    	<div class="row-new">
 
+			<?php 
+			// Start the loop.
+			while(have_posts()) : the_post(); 
+			?>
 
-
-	<!-- header -->
-    <div class="section-header">
-        <div class="container">
-            <div class="row">
-                <div class="col">
-                    <h1 class="animated fadeInUp" data-animation="fadeInUp"><?php the_title(); ?></h1>
-					
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- / header -->
-
-    <!-- content -->
-    <div class="section-body">
-        <div class="container">
-
-            <div class="row">
-                <div class="col">
-					<div class="image" style="text-align: center; margin-bottom: 20px;"><?php the_post_thumbnail(); ?></div>
-                    <div class="animated fadeIn card single-portfolio" style="animation-delay: .1s;" data-animation="fadeIn">
-                        <div class="card-block">
-							<div class="content"><?php the_content(); ?></div>
+			<!-- Title -->
+			<div class="column column-1">
+				<h2 class="section-header animated fadeInUp" data-animation="fadeInUp"><?php the_title(); ?></h1>
+			</div>
+			
+			<!-- Meta -->
+			<div class="column column-1">
+				<div class="post-meta">
+					<div class="post-meta-item date"><span class="label">Date </span><?php the_date(); ?></div>
+					<div class="post-meta-item agency"><span class="label">Agency </span><?php echo post_custom( $key = 'agency' ); ?></div>
+					<div class="post-meta-item client"><span class="label">Client </span><?php echo post_custom( $key = 'client' ); ?></div>
+				</div>
+			</div>
+				
+			<!-- Images -->
+			<div class="column column-2" style="justify-content: flex-start;">
+				<div class="thumbnail">
+					<!-- <div class="row">
+						<div class="column column-1"> -->
+							<?php the_post_thumbnail($post->ID,'small'); ?>
+						<!-- </div>
+					</div> -->
+				</div>
+				<div class="portfolio">
+					<div class="row-new" style="width:auto;margin:0 -15px;">
+						<?php 
+						// acf_photo_gallery('additional_images',$post->ID); 
+						//Get the images ids from the post_metadata
+						$images = acf_photo_gallery('additional_images', $post->ID);
+						//Check if return array has anything in it
+						if( count($images) ):
+							//Cool, we got some data so now let's loop over it
+							foreach($images as $image):
+								$id = $image['id']; // The attachment id of the media
+								$title = $image['title']; //The title
+								$caption= $image['caption']; //The caption
+								$full_image_url= $image['full_image_url']; //Full size image url
+								$custom_thumbnail_image_url = acf_photo_gallery_resize_image($full_image_url, 360, 200); //Resized size to 262px width by 160px height image url
+								// $thumbnail_image_url= $image['thumbnail_image_url']; //Get the thumbnail size image url 150px by 150px
+								// $url= $image['url']; //Goto any link when clicked
+								// $target= $image['target']; //Open normal or new tab
+								$alt = get_field('photo_gallery_alt', $id);
+								$class = get_field('photo_gallery_class', $id);
+						?>
+						<div class="column column-2">
+							<div class="portfolio-item">
+								<?php if( !empty($full_image_url) ){ ?><a href="<?php echo $full_image_url; ?>" target="_blank" class="media"><?php } ?>
+									<img src="<?php echo $custom_thumbnail_image_url; ?>" alt="<?php echo $title; ?>" title="<?php echo $title; ?>">
+									<?php if( !empty($full_image_url) ){ ?></a><?php } ?>
+								<div class="meta">
+									<?php echo $caption; ?>
+								</div>
+							</div>
 						</div>
-						<div class="meta" style="padding:0 20px; margin-bottom:20px;">
-							<div class="date"><?php the_date(); ?></div>
-							<div class="agency">Agency: <?php echo post_custom( $key = 'agency' )[0]; ?></div>
-							<div class="client">Client: <?php echo post_custom( $key = 'client' )[0]; ?></div>
-						</div>
-                    </div>
-                </div>
-            </div>
+						<?php 
+						endforeach; 
+						endif; 
+						?>
+					</div>
+				</div>
+			</div>
+			
+			<!-- Text -->
+			<div class="column column-2" style="justify-content: flex-start;">
+				<div class="content animated" style="animation-delay: .1s;" data-animation="fadeIn">
+					<?php the_content(); ?>
+				</div>
+				<div class="categories animated" style="animation-delay: .2s;" data-animation="fadeIn">
+					<h4>Categories</h4>
+					<?php the_category(); ?>
+				</div>
+				<div class="tags animated" style="animation-delay: .3s;" data-animation="fadeIn">
+					<h4>Tags</h4>
+					<?php the_tags('<ul class="post-tags"><li>','</li><li>','</li></ul>'); ?>
+				</div>
+			</div>
 
-        </div>
-    </div>
-    <!-- / content -->
+			<!-- Post Navigation -->
+			<div class="column column-1">
+				<?php 
+				the_post_navigation(
+					array(
+						'next_text' => '<span class="label">Next: </span>%title',
+						'prev_text' => '<span class="label">Previous: </span>%title',
+						)
+					);
+					?>
+			</div>
 
-	<?php 
-	the_category();
-	the_tags('<ul class="post-tags"><li>','</li><li>','</li></ul>');
-	
-		// Previous/next post navigation.
-		the_post_navigation(
-			array(
-				'next_text' => '<span class="meta-nav" aria-hidden="true">Next</span> ' .
-					'<span class="screen-reader-text">Next post:</span> ' .
-					'<span class="post-title">%title</span>',
-				'prev_text' => '<span class="meta-nav" aria-hidden="true">Previous</span> ' .
-					'<span class="screen-reader-text">Previous post:</span> ' .
-					'<span class="post-title">%title</span>',
-			)
-		);
+			<?php 
+			// End the loop.
+			endwhile;
+			?>
 
-		// End the loop.
-		endwhile;
-	?>
-
+		</div>
+	</div>
 </section>
 <!-- / about -->
 
