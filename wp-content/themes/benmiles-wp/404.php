fb7a2adfd7bb8eff51cceec0a63db420bbf9e713
Ben Miles <bcgm3studios@gmail.com>
2022-08-04 18:16:48 -0400

    404 Page
    Improving the layout for this 404 Error Page

--- wp-content/themes/benmiles-wp/404.php
+++ wp-content/themes/benmiles-wp/404.php
@@ -7,39 +7,25 @@ get_header();
 ?>
 
 <!-- 404 -->
-<section id="404">
-
-    <!-- header -->
-    <div class="section-header">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-7 col-xl-8">
-                    <h2 class="animated" data-animation="fadeInUp">404</h2>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- / header -->
-
-    <!-- content -->
-    <div class="section-body">
-        <div class="container">
-
-            <div class="row">
-                <div class="col-sm-7 col-xl-8">
-                    <div class="animated card error-404" style="animation-delay: .1s;" data-animation="fadeInUp">
-                        <div class="card-block">
-                            <p class="card-text">Page not found.</p>
-                            <p class="card-text">Go to home page? Search?</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-        </div>
-    </div>
-    <!-- / content -->
-
+<section id="error-404">
+	<div class="container">
+		<div class="row">
+			<div class="column column-1">
+				
+				<!-- header -->
+				<h1 class="section-header animated" data-animation="fadeInUp">404</h1>
+				
+				<!-- content -->
+				<div class="animated card error-404" style="animation-delay: .1s;" data-animation="fadeInUp">
+					<div class="card-block">
+						<p class="card-text">Page not found.</p>
+						<p class="card-text">Go to <a href="<?php bloginfo('url'); ?>" target="_self">home page</a>?</p>
+					</div>
+				</div>
+				
+			</div>	
+		</div>
+	</div>
 </section>
 <!-- / about -->
 
