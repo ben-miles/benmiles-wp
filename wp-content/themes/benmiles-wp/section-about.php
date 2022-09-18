f3a334dcbef6c2266369ff312ae41ccdde135057
Ben Miles <bcgm3studios@gmail.com>
2022-09-18 14:44:01 -0400

    Simplified About Section for Home Page
    Hiding the icons all the proficiencies for now. I think I want them to show on the dedicated About page, but not on the Home page -- Just seems like too much content for that purpose.

--- wp-content/themes/benmiles-wp/section-about.php
+++ wp-content/themes/benmiles-wp/section-about.php
@@ -7,58 +7,62 @@ Template Name: Section - About
 
 <!-- about -->
 <section id="about">
-	<div class="container-new">
+	<div class="container-new" style="height:100%;">
 		<div class="row-new">
 			<div class="column column-1">
 
-				<h2 class="section-header animated" data-animation="fadeInUp">About</h2>
+				<h2 class="section-header animated" data-animation="fadeInUp">About Ben Miles</h2>
 
 				<div class="animated bio" style="animation-delay: .1s;" data-animation="fadeInUp">
-					<p><strong>I am an experienced and well-rounded graphic designer and web developer located in central Florida.</strong> 
-						<br><strong>For as long as I've been able, I have been creating.</strong></p>
+					<p><b>I am an experienced and well-rounded graphic designer and web developer located in central Florida.</b></p>
 					<p>I've freelanced, incorporated and run my own design company with a brick-and-mortar location, worked lead positions for print and web agencies, both as an artist and developer, both in-person and remotely. I have always enjoyed the juxtaposition of art and engineering that a career in commercial design offers. </p>
 					<p>Outside of work, I'm an avid gardener, a homebrewer, a musician and a movie buff. </p>
 				</div>
+				<p style="text-align:center">
 					<a href="<?php bloginfo('url'); ?>/about" target="_self" class="cta-button">Learn more about me</a>
+				</p>
+
+				<!-- TODO: SHOW all of this stuff on the About page, but HIDE on the Home page -->
+				<!-- TODO: Update the text content above to reflect knowledge of these items, briefly.  -->
 
 				<!-- code languages -->
-				<h3>Code Languages</h3>
-				<div class="svg-row">
+				<!-- <h3>Code Languages</h3> -->
+				<!-- <div class="svg-row">
 					<?php
-					displaySVG( 'HTML 5' );
-					displaySVG( 'CSS 3', .1 );
-					displaySVG( 'JavaScript', .2 );
-					displaySVG( 'PHP', .3 );
-					displaySVG( 'mySQL', .4 );
+					// displaySVG( 'HTML 5' );
+					// displaySVG( 'CSS 3', .1 );
+					// displaySVG( 'JavaScript', .2 );
+					// displaySVG( 'PHP', .3 );
+					// displaySVG( 'mySQL', .4 );
 					?>
-				</div>
+				</div> -->
 				<!-- frameworks, libraries -->
-				<h3>Frameworks & Libraries</h3>
-				<div class="svg-row">
+				<!-- <h3>Frameworks & Libraries</h3> -->
+				<!-- <div class="svg-row">
 					<?php
-					displaySVG( 'Bootstrap', .1 );
-					displaySVG( 'jQuery', .2 );
-					displaySVG( 'Joomla!', .3 );
+					// displaySVG( 'Bootstrap', .1 );
+					// displaySVG( 'jQuery', .2 );
+					// displaySVG( 'Joomla!', .3 );
 					?>
-				</div>
-				<div class="svg-row">
+				</div> -->
+				<!-- <div class="svg-row">
 					<?php
-					displaySVG( 'Vue', .4 );
-					displaySVG( 'Laravel', .5 );
-					displaySVG( 'WordPress', .6 );
+					// displaySVG( 'Vue', .4 );
+					// displaySVG( 'Laravel', .5 );
+					// displaySVG( 'WordPress', .6 );
 					?>
-				</div>
+				</div> -->
 				<!-- software -->
-				<h3>Software</h3>
-				<div class="svg-row">
+				<!-- <h3>Software</h3> -->
+				<!-- <div class="svg-row">
 					<?php
-					displaySVG( 'phpStorm', .1 );
-					displaySVG( 'Photoshop', .2 );
-					displaySVG( 'Illustrator', .3 );
-					displaySVG( 'inDesign', .4 );
-					displaySVG( 'VS Code', .4 );
+					// displaySVG( 'phpStorm', .1 );
+					// displaySVG( 'Photoshop', .2 );
+					// displaySVG( 'Illustrator', .3 );
+					// displaySVG( 'inDesign', .4 );
+					// displaySVG( 'VS Code', .4 );
 					?>
-				</div>
+				</div> -->
 
             </div>
         </div>
