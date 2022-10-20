e0ffd5a94e4eaf4b99d88b0eeaa8bc90756949db
Ben Miles <bcgm3studios@gmail.com>
2022-10-20 13:06:32 -0400

    Plugins Update

--- wp-content/themes/benmiles-wp/single-portfolio.php
+++ wp-content/themes/benmiles-wp/single-portfolio.php
@@ -36,36 +36,23 @@ get_header();
 			<!-- Images -->
 			<div class="column column-2" style="justify-content: flex-start;">
 				<div class="thumbnail">
-					<!-- <div class="row">
-						<div class="column column-1"> -->
-							<?php the_post_thumbnail($post->ID,'small'); ?>
-						<!-- </div>
-					</div> -->
+					<?php the_post_thumbnail($post->ID,'small'); ?>
 				</div>
 				<div class="portfolio">
 					<div class="row" style="width:auto;margin:0 -15px;">
 						<?php 
-						// acf_photo_gallery('additional_images',$post->ID); 
-						//Get the images ids from the post_metadata
 						$images = acf_photo_gallery('additional_images', $post->ID);
-						//Check if return array has anything in it
 						if( count($images) ):
-							//Cool, we got some data so now let's loop over it
 							foreach($images as $image):
-								$id = $image['id']; // The attachment id of the media
-								$title = $image['title']; //The title
-								$caption= $image['caption']; //The caption
-								$full_image_url= $image['full_image_url']; //Full size image url
-								$custom_thumbnail_image_url = acf_photo_gallery_resize_image($full_image_url, 360, 200); //Resized size to 262px width by 160px height image url
-								// $thumbnail_image_url= $image['thumbnail_image_url']; //Get the thumbnail size image url 150px by 150px
-								// $url= $image['url']; //Goto any link when clicked
-								// $target= $image['target']; //Open normal or new tab
-								$alt = get_field('photo_gallery_alt', $id);
-								$class = get_field('photo_gallery_class', $id);
+								$id = $image['id'];
+								$title = $image['title'];
+								$caption = $image['caption'];
+								$full_image_url = $image['full_image_url'];
+								$custom_thumbnail_image_url = acf_photo_gallery_resize_image($full_image_url, 360, 200);
 						?>
 						<div class="column column-2">
 							<div class="portfolio-item">
-								<?php if( !empty($full_image_url) ){ ?><a href="<?php echo $full_image_url; ?>" target="_blank" class="media"><?php } ?>
+								<?php if( !empty($full_image_url) ){ ?><a href="<?php echo $full_image_url; ?>" class="foobox"><?php } ?>
 									<img src="<?php echo $custom_thumbnail_image_url; ?>" alt="<?php echo $title; ?>" title="<?php echo $title; ?>">
 									<?php if( !empty($full_image_url) ){ ?></a><?php } ?>
 								<div class="meta">
