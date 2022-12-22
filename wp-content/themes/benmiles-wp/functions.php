0ae141ff5a369149d74aa423b663619b8e8fa3fb
Ben Miles <bcgm3studios@gmail.com>
2022-12-22 13:30:04 -0500

    Remove Emoji
    Not using them, bye bye.

--- wp-content/themes/benmiles-wp/functions.php
+++ wp-content/themes/benmiles-wp/functions.php
@@ -17,9 +17,39 @@ add_filter('excerpt_length', 'shorten_auto_excerpts');
 // Via https://stackoverflow.com/a/49025096/6853842
 add_filter('wpcf7_autop_or_not', '__return_false');
 
+// Remove Emojis
+// Via https://kinsta.com/knowledgebase/disable-emojis-wordpress/
+function disable_emojis() {
+	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+	remove_action( 'wp_print_styles', 'print_emoji_styles' );
+	remove_action( 'admin_print_styles', 'print_emoji_styles' ); 
+	remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+	remove_filter( 'comment_text_rss', 'wp_staticize_emoji' ); 
+	remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+	add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
+	add_filter( 'wp_resource_hints', 'disable_emojis_remove_dns_prefetch', 10, 2 );
+}
+add_action( 'init', 'disable_emojis' );
+function disable_emojis_tinymce( $plugins ) {
+	if ( is_array( $plugins ) ) {
+		return array_diff( $plugins, array( 'wpemoji' ) );
+	} else {
+		return array();
+	}
+}
+function disable_emojis_remove_dns_prefetch( $urls, $relation_type ) {
+if ( 'dns-prefetch' == $relation_type ) {
+	$emoji_svg_url = apply_filters( 'emoji_svg_url', 'https://s.w.org/images/core/emoji/2/svg/' );
+	$urls = array_diff( $urls, array( $emoji_svg_url ) );
+}
+return $urls;
+}
+
+// FRONTEND ONLY:
 if( !is_admin() ){
 
-	// Remove Gutenberg Block Library CSS from loading on the frontend
+	// Remove Gutenberg Block Library CSS
 	function smartwp_remove_wp_block_library_css(){
 		wp_dequeue_style( 'wp-block-library' );
 		wp_dequeue_style( 'wp-block-library-theme' );
