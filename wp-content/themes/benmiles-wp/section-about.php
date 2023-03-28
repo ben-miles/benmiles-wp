a78cae68b08d7904535e408d97f22fcdfbcd322c
Ben Miles <bcgm3studios@gmail.com>
2023-03-28 12:46:29 -0400

    Improve Bio
    With some help from predictive text AI, I wrote a shorter bio for the Home page and a longer version for the About page.

--- wp-content/themes/benmiles-wp/section-about.php
+++ wp-content/themes/benmiles-wp/section-about.php
@@ -12,27 +12,29 @@ Template Name: Section - About
 			<div class="column column-12">
 
 				<h2 class="heading animated" data-animation="fadeIn">About Ben Miles</h2>
-
-				<div class="animated card" data-animation="fadeIn" style="animation-delay: .1s;">
-					<p><b>I'm a graphic designer and full-stack web developer, located in central Florida, with 20 years of experience.</b></p>
-					<p>I have always found joy in the balance of creative and problem-solving tasks that my career offers.</p>
-					<p>I've freelanced, incorporated and run my own design company, and worked lead positions in both design and development agencies.</p>
-					<p>Outside of work, I prefer to spend time homebrewing, screenprinting, or gardening.</p>
-				</div>
 				
-				<!-- <p style="text-align:center"> -->
-					<?php 
-					if( is_front_page() ){
-						// Display this content on the Home Page ?>
-						<div class="button-group">
-							<a href="#portfolio" target="_self" class="button cta animated" data-animation="fadeInUp" style="animation-delay: .2s;"><span>My Work</span><?php echo displaySVG('down'); ?></a>
-							<a href="<?php echo home_url(); ?>/about" target="_self" class="button cta secondary animated" data-animation="fadeInUp" style="animation-delay: .3s;"><span>More About Me</span><?php echo displaySVG('right'); ?></a>
-						</div>
-					<?php } else { 
-						// Display this content on the About Page ?>
-						<a href="#experience-and-skills" target="_self" class="button cta animated" data-animation="fadeInUp" style="animation-delay: .2s;"><span>Experience & Skills</span><?php echo displaySVG('down', 'bounce'); ?></a>
-					<?php } ?>
-				<!-- </p> -->
+				<?php 
+				if( is_front_page() ){
+					// Display this content on the Home Page ?>
+					<div class="animated card" data-animation="fadeIn" style="animation-delay: .1s;">
+						<p>I am a highly-skilled and creative individual, with a passion for functional design. I have over fifteen years of working experience in graphic design, and almost ten in full-stack web development.</p>
+						<p>Combining my design and programming skills, I create interactive and engaging web sites and applications that not only look great, but also function intuitively.</p>
+					</div>
+					<div class="button-group">
+						<a href="#portfolio" target="_self" class="button cta animated" data-animation="fadeInUp" style="animation-delay: .2s;"><span>My Work</span><?php echo displaySVG('down'); ?></a>
+						<a href="<?php echo home_url(); ?>/about" target="_self" class="button cta secondary animated" data-animation="fadeInUp" style="animation-delay: .3s;"><span>More About Me</span><?php echo displaySVG('right'); ?></a>
+					</div>
+				<?php } else { 
+					// Display this content on the About Page ?>
+					<div class="animated card" data-animation="fadeIn" style="animation-delay: .1s;">
+						<p>I am a highly-skilled and creative individual, with a passion for design and technology. I have over fifteen years of working experience in graphic design, and almost ten in full-stack web development.</p>
+						<p>I started my career as a graphic designer, but I was always interested in the technical side of things. I taught myself how to make web sites, and eventually made the switch to a full-stack web development role.</p>
+						<p>I've freelanced, started my own web and print design company, lead design and production departments in an international print shop chain, and developed a variety of new and legacy applications in multiple web agencies.</p>
+						<p>Combining my design and programming skills, I create interactive and engaging web sites and applications that not only look great, but also function intuitively.</p>
+						<p>Outside of work, you'll most likely find me gardening, homebrewing, or screen printing.</p>
+					</div>
+					<a href="#experience-and-skills" target="_self" class="button cta animated" data-animation="fadeInUp" style="animation-delay: .2s;"><span>Experience & Skills</span><?php echo displaySVG('down', 'bounce'); ?></a>
+				<?php } ?>
 
             </div>
         </div>
