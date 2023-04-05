f16e09c641b52a6fd6a9a18a86242de667b3eacd
Ben Miles <bcgm3studios@gmail.com>
2023-04-05 15:07:27 -0400

    Simplified Work History Styles
    Also replaced the hardcoded work history to be more programmatic, for easier updating in the future.

--- wp-content/themes/benmiles-wp/section-experience-and-skills.php
+++ wp-content/themes/benmiles-wp/section-experience-and-skills.php
@@ -2,7 +2,68 @@
 /*
 Template Name: Section - Experience & Skills
 */
-// error_reporting(0);
+
+$jobs = [
+	[
+		'title' => 'Art Director & Web Developer',
+		'company' => 'Shire Digital Solutions',
+		'link' => 'https://shire-digital.com/',
+		'dates'	=> 'May 2018 - Present',
+		'location' => 'Remote'
+	],
+	[
+		'title' => 'Systems Analyst & Graphic Designer',
+		'company' => 'PIP Printing & Marketing',
+		'link' => 'https://pipbrevard.com/',
+		'dates'	=> 'Nov. 2017 - May 2018',
+		'location' => 'Merritt Island, FL'
+	],
+	[
+		'title' => 'Web Developer & Graphic Designer',
+		'company' => 'furryLogic',
+		'link' => 'https://furrylogic.net/',
+		'dates'	=> 'Nov. 2015 - Nov. 2017',
+		'location' => 'Cocoa Village, FL'
+	],
+	[
+		'title' => 'Graphic Designer, Pre-Press Specialist, Large-Format Production & Installation Lead',
+		'company' => 'PIP Printing & Marketing',
+		'link' => 'https://pipbrevard.com/',
+		'dates'	=> 'Feb. 2012 - Nov. 2015',
+		'location' => 'Merritt Island, FL'
+	],
+	[
+		'title' => 'Graphic Designer, Web Support',
+		'company' => 'Space Coast Coupons',
+		'link' => 'https://spacecoast.coupons/',
+		'dates'	=> 'Nov. 2011 - Feb. 2012',
+		'location' => 'Cocoa Village, FL'
+	],
+	[
+		'title' => 'Owner, Graphic Designer, Web Developer',
+		'company' => 'BCGM3 Studios',
+		'link' => 'https://bcgm3.com/',
+		'dates'	=> 'Jan. 2009 - Nov. 2011',
+		'location' => 'Cocoa Village, FL'
+	]
+];
+
+$schools = [
+	[
+		'title' => 'Bachelors of Fine Arts in Graphic Design',
+		'company' => 'University of Central Florida',
+		'link' => 'http://svad.cah.ucf.edu/',
+		'dates'	=> '2006 - 2009',
+		'location' => 'Orlando, FL'
+	],
+	[
+		'title' => 'Associates of Arts with a Concentration in Design',
+		'company' => 'Eastern Florida State College',
+		'link' => 'http://easternflorida.edu/',
+		'dates'	=> '2004 - 2006',
+		'location' => 'Cocoa, FL'
+	]
+];
 ?>
 
 <!-- experience-and-skills -->
@@ -15,120 +76,48 @@ Template Name: Section - Experience & Skills
 
 				<!-- experience -->
 				<div class="column column-9 experience">
-					<h2 class="heading animated" data-animation="fadeIn">Experience & Education</h2>
+					<h2 class="heading animated" data-animation="fadeIn">Experience</h2>
                     <ul class="timeline animated" data-animation="fadeIn" style="animation-delay:0.1s;">
 
-                        <li class="timeline-item timeline-inverted animated" data-animation="fadeInUp">
-                            <div class="timeline-badge">
-								<?php echo displaySVG('work'); ?>
-                            </div>
-                            <div class="timeline-panel">
-								<h4 class="timeline-title">Art Director & Web Developer</h4>
-								<ul class="timeline-details">
-									<li><?php echo displaySVG('external-link'); ?><a href="https://shire-digital.com/" target="_blank">Shire Digital Solutions</a></li>
-									<li><?php echo displaySVG('calendar'); ?>May 2018 - Present</li>
-									<li><?php echo displaySVG('map-pin'); ?>Cocoa Beach, FL</li>
-								</ul>
-                            </div>
-                        </li>
+						<?php
+							foreach($jobs as $job) {
+								echo '<li class="timeline-item animated" data-animation="fadeInUp">
+										<div class="timeline-badge">
+											' . displaySVG('work') . '
+										</div>
+										<div class="timeline-panel">
+											<h4 class="timeline-title">' . $job['title'] . '</h4>
+											<ul class="timeline-details">
+												<li>' . displaySVG('external-link') . '<a href="' . $job['link'] . '" target="_blank">' . $job['company'] . '</a></li>
+												<li>' . displaySVG('calendar') . $job['dates'] . '</li>
+												<li>' . displaySVG('map-pin') . $job['location'] . '</li>
+											</ul>
+										</div>
+									</li>';
+							}
+						?>
 
-						<li class="timeline-item animated" data-animation="fadeInUp">
-                            <div class="timeline-badge">
-								<?php echo displaySVG('work'); ?>
-                            </div>
-                            <div class="timeline-panel">
-								<h4 class="timeline-title">Systems Analyst & Graphic Designer</h4>
-								<ul class="timeline-details">
-									<li><?php echo displaySVG('external-link'); ?><a href="http://pipbrevard.com/" target="_blank">PIP Printing & Marketing</a></li>
-									<li><?php echo displaySVG('calendar'); ?>Nov. 2017 - May 2018</li>
-									<li><?php echo displaySVG('map-pin'); ?>Merritt Island, FL</li>
-								</ul>
-                            </div>
-                        </li>
+					</ul>
+					<h2 class="heading animated" data-animation="fadeIn">Education</h2>
+                    <ul class="timeline animated" data-animation="fadeIn" style="animation-delay:0.1s;">
 
-                        <li class="timeline-item timeline-inverted animated" data-animation="fadeInUp">
-                            <div class="timeline-badge">
-								<?php echo displaySVG('work'); ?>
-                            </div>
-                            <div class="timeline-panel">
-								<h4 class="timeline-title">Web Developer & Graphic Designer</h4>
-								<ul class="timeline-details">
-									<li><?php echo displaySVG('external-link'); ?><a href="http://furrylogic.net/" target="_blank">furryLogic</a></li>
-									<li><?php echo displaySVG('calendar'); ?>Nov. 2015 - Nov. 2017</li>
-									<li><?php echo displaySVG('map-pin'); ?>Cocoa Village, FL</li>
-								</ul>
-                            </div>
-                        </li>
-
-                        <li class="timeline-item animated" data-animation="fadeInUp">
-                            <div class="timeline-badge">
-								<?php echo displaySVG('work'); ?>
-                            </div>
-                            <div class="timeline-panel">
-								<h4 class="timeline-title">Graphic Designer, Pre-Press Specialist, Large-Format Production & Installation Lead</h4>
-								<ul class="timeline-details">
-									<li><?php echo displaySVG('external-link'); ?><a href="http://pipbrevard.com/" target="_blank">PIP Printing & Marketing</a></li>
-									<li><?php echo displaySVG('calendar'); ?>Feb. 2012 - Nov. 2015</li>
-									<li><?php echo displaySVG('map-pin'); ?>Merritt Island, FL</li>
-								</ul>
-                            </div>
-                        </li>
-
-                        <li class="timeline-item timeline-inverted animated" data-animation="fadeInUp">
-                            <div class="timeline-badge">
-								<?php echo displaySVG('work'); ?>
-                            </div>
-                            <div class="timeline-panel">
-								<h4 class="timeline-title">Graphic Designer, Web Support</h4>
-								<ul class="timeline-details">
-									<li><?php echo displaySVG('external-link'); ?><a href="http://spacecoast.coupons/" target="_blank">Space Coast Coupons</a></li>
-									<li><?php echo displaySVG('calendar'); ?>Nov. 2011 - Feb. 2012</li>
-									<li><?php echo displaySVG('map-pin'); ?>Merritt Island, FL</li>
-								</ul>
-                            </div>
-                        </li>
-
-                        <li class="timeline-item animated" data-animation="fadeInUp">
-                            <div class="timeline-badge">
-								<?php echo displaySVG('work'); ?>
-                            </div>
-                            <div class="timeline-panel">
-								<h4 class="timeline-title">Owner, Graphic Designer, Web Developer</h4>
-								<ul class="timeline-details">
-									<li><?php echo displaySVG('external-link'); ?><a href="http://bcgm3.com/" target="_blank">BCGM3 Studios</a></li>
-									<li><?php echo displaySVG('calendar'); ?>Apr. 2010 - Feb. 2012</li>
-									<li><?php echo displaySVG('map-pin'); ?>Merritt Island, FL</li>
-								</ul>
-                            </div>
-                        </li>
-
-                        <li class="timeline-item timeline-inverted animated" data-animation="fadeInUp">
-                            <div class="timeline-badge">
-								<?php echo displaySVG('school'); ?>
-                            </div>
-                            <div class="timeline-panel">
-								<h4 class="timeline-title">Bachelors of Fine Arts in Graphic Design</h4>
-								<ul class="timeline-details">
-									<li><?php echo displaySVG('external-link'); ?><a href="http://svad.cah.ucf.edu/" target="_blank">University of Central Florida</a></li>
-									<li><?php echo displaySVG('calendar'); ?>2006 - 2009</li>
-									<li><?php echo displaySVG('map-pin'); ?>Orlando, FL</li>
-								</ul>
-                            </div>
-                        </li>
-
-                        <li class="timeline-item animated" data-animation="fadeInUp">
-                            <div class="timeline-badge">
-								<?php echo displaySVG('school'); ?>
-                            </div>
-                            <div class="timeline-panel">
-								<h4 class="timeline-title">Associates of Arts with a Concentration in Design</h4>
-								<ul class="timeline-details">
-									<li><?php echo displaySVG('external-link'); ?><a href="http://easternflorida.edu/" target="_blank">Eastern Florida State College</a></li>
-									<li><?php echo displaySVG('calendar'); ?>2004 - 2006</li>
-									<li><?php echo displaySVG('map-pin'); ?>Cocoa, FL</li>
-								</ul>
-                            </div>
-                        </li>
+					<?php
+						foreach($schools as $school) {
+							echo '<li class="timeline-item animated" data-animation="fadeInUp">
+									<div class="timeline-badge">
+										' . displaySVG('work') . '
+									</div>
+									<div class="timeline-panel">
+										<h4 class="timeline-title">' . $school['title'] . '</h4>
+										<ul class="timeline-details">
+											<li>' . displaySVG('external-link') . '<a href="' . $school['link'] . '" target="_blank">' . $school['company'] . '</a></li>
+											<li>' . displaySVG('calendar') . $school['dates'] . '</li>
+											<li>' . displaySVG('map-pin') . $school['location'] . '</li>
+										</ul>
+									</div>
+								</li>';
+						}
+					?>
 
                     </ul>
                 </div>
