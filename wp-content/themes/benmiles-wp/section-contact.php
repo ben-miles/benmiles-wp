e13a14900ae21602f20c74c717a4df695242cd03
Ben Miles <bcgm3studios@gmail.com>
2024-06-18 15:23:42 -0400

    Update Contact Section to use Fluent Forms

--- wp-content/themes/benmiles-wp/section-contact.php
+++ wp-content/themes/benmiles-wp/section-contact.php
@@ -12,7 +12,7 @@ Template Name: Section - Contact
 				<h2 class="heading animated" data-animation="fadeIn">Contact</h2>
 				<p class="animated" data-animation="fadeIn" style="animation-delay:0.1s;">Send me a message with the form below.</p>
 				<div class="form-wrapper">
-					<?php echo do_shortcode( '[contact-form-7 id="381" title="Contact Form"]' ); ?>		
+					<?php echo do_shortcode( '[fluentform id="3"]' ); ?>
 				</div>
 			</div>
 		</div>
