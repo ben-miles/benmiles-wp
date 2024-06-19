fc13b9c2490b1fd401fdce78978f10b2eed4b2a4
Ben Miles <bcgm3studios@gmail.com>
2024-06-18 21:55:45 -0400

    Unify White Space in functions.php

--- wp-content/themes/benmiles-wp/functions.php
+++ wp-content/themes/benmiles-wp/functions.php
@@ -7,7 +7,7 @@ add_theme_support('post-thumbnails');
 add_theme_support('title-tag');
 
 // Disable automatic image scaling
-add_filter( 'big_image_size_threshold', '__return_false' );
+add_filter('big_image_size_threshold', '__return_false');
 
 // Shorten Automatic Excerpts
 // Via https://www.hostinger.com/tutorials/wordpress-excerpt-length
@@ -17,10 +17,11 @@ function shorten_auto_excerpts($length){
 add_filter('excerpt_length', 'shorten_auto_excerpts');
 
 // Change "More" Indicator on Auto Excerpts
-function excerpt_more_indicator( $more ) {
+function excerpt_more_indicator($more){
 	return '...';
 }
-add_filter( 'excerpt_more', 'excerpt_more_indicator' );
+add_filter('excerpt_more', 'excerpt_more_indicator');
+
 // Remove Comments
 add_action('admin_init', function(){
 	// Redirect any user trying to access comments page
@@ -61,80 +62,74 @@ add_filter('wpcf7_autop_or_not', '__return_false');
 
 // Remove Gutenberg / Block Library CSS
 function remove_wp_block_library_css(){
-	wp_dequeue_style( 'wp-block-library' );
-	wp_dequeue_style( 'wp-block-library-theme' );
-	wp_dequeue_style( 'wc-blocks-style' );
-	wp_dequeue_style( 'global-styles' );
-
+	wp_dequeue_style('wp-block-library');
+	wp_dequeue_style('wp-block-library-theme');
+	wp_dequeue_style('wc-blocks-style');
+	wp_dequeue_style('global-styles');
 } 
-add_action( 'wp_enqueue_scripts', 'remove_wp_block_library_css', 100 );
+add_action('wp_enqueue_scripts', 'remove_wp_block_library_css', 100);
 
 // Remove Emojis
 // Via https://kinsta.com/knowledgebase/disable-emojis-wordpress/
-function remove_emojis() {
-	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
-	remove_action( 'wp_print_styles', 'print_emoji_styles' );
-	remove_action( 'admin_print_styles', 'print_emoji_styles' ); 
-	remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
-	remove_filter( 'comment_text_rss', 'wp_staticize_emoji' ); 
-	remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
-	add_filter( 'tiny_mce_plugins', 'remove_emojis_tinymce' );
-	add_filter( 'wp_resource_hints', 'remove_emojis_remove_dns_prefetch', 10, 2 );
+function remove_emojis(){
+	remove_action('wp_head', 'print_emoji_detection_script', 7);
+	remove_action('admin_print_scripts', 'print_emoji_detection_script');
+	remove_action('wp_print_styles', 'print_emoji_styles');
+	remove_action('admin_print_styles', 'print_emoji_styles'); 
+	remove_filter('the_content_feed', 'wp_staticize_emoji');
+	remove_filter('comment_text_rss', 'wp_staticize_emoji'); 
+	remove_filter('wp_mail', 'wp_staticize_emoji_for_email');
+	add_filter('tiny_mce_plugins', 'remove_emojis_tinymce');
+	add_filter('wp_resource_hints', 'remove_emojis_remove_dns_prefetch', 10, 2);
 }
-add_action( 'init', 'remove_emojis' );
-function remove_emojis_tinymce( $plugins ) {
-	if ( is_array( $plugins ) ) {
-		return array_diff( $plugins, array( 'wpemoji' ) );
+add_action('init', 'remove_emojis');
+function remove_emojis_tinymce($plugins){
+	if(is_array($plugins)){
+		return array_diff($plugins, array('wpemoji'));
 	} else {
 		return array();
 	}
 }
-function remove_emojis_remove_dns_prefetch( $urls, $relation_type ) {
-if ( 'dns-prefetch' == $relation_type ) {
-	$emoji_svg_url = apply_filters( 'emoji_svg_url', 'https://s.w.org/images/core/emoji/2/svg/' );
-	$urls = array_diff( $urls, array( $emoji_svg_url ) );
-}
-return $urls;
+function remove_emojis_remove_dns_prefetch($urls, $relation_type){
+	if('dns-prefetch' == $relation_type){
+		$emoji_svg_url = apply_filters('emoji_svg_url', 'https://s.w.org/images/core/emoji/2/svg/');
+		$urls = array_diff($urls, array($emoji_svg_url));
+	}
+	return $urls;
 }
 
 // Load Custom Scripts and Styles
-function load_custom_scripts_and_styles() {
-
+function load_custom_scripts_and_styles(){
 	// Make sure the custom script gets a `type` attribute set to `module`
-	add_filter( 'script_loader_tag', 'add_type_to_script', 10, 3 );
-	function add_type_to_script( $tag, $handle, $src ) {
-		if ( $handle === 'custom' ) {
-			$tag = '<script type="module" src="' . esc_url( $src ) . '"></script>';
+	add_filter('script_loader_tag', 'add_type_to_script', 10, 3);
+	function add_type_to_script($tag, $handle, $src){
+		if($handle === 'custom'){
+			$tag = '<script type="module" src="' . esc_url($src) . '"></script>';
 		}
 		return $tag;
 	}
-
 	// Load Styles
-	if(is_singular( 'portfolio' )){
+	if(is_singular('portfolio')){
 		// Single Portfolio Pages Only:
-		wp_enqueue_style( 'glightbox', get_template_directory_uri() . '/assets/css/glightbox.min.css', array(), null, false );
-		wp_enqueue_script( 'glightbox', get_template_directory_uri() . '/assets/js/glightbox.min.js', array(), null, false );
+		wp_enqueue_style('glightbox', get_template_directory_uri() . '/assets/css/glightbox.min.css', array(), null, false);
+		wp_enqueue_script('glightbox', get_template_directory_uri() . '/assets/js/glightbox.min.js', array(), null, false);
 	}
-	wp_enqueue_style( 'custom', get_template_directory_uri() . '/assets/css/custom.css', false, '', 'all' );
-
+	wp_enqueue_style('custom', get_template_directory_uri() . '/assets/css/custom.css', false, '', 'all');
 	// Load Scripts
-	wp_enqueue_script( 'recaptcha', 'https://www.google.com/recaptcha/api.js?render=' . GOOGLE_RECAPTCHA_SITE_KEY, array(), null, false );
-	if(is_page( 'Portfolio' )){
+	wp_enqueue_script('recaptcha', 'https://www.google.com/recaptcha/api.js?render=' . GOOGLE_RECAPTCHA_SITE_KEY, array(), null, false);
+	if(is_page('Portfolio')){
 		// Portfolio Page Only:
-		wp_enqueue_script( 'imagesloaded', get_template_directory_uri() . '/assets/js/imagesloaded.min.js', array(), null, false );
-		wp_enqueue_script( 'isotope', get_template_directory_uri() . '/assets/js/isotope.min.js', array('imagesloaded'), null, false );
+		wp_enqueue_script('imagesloaded', get_template_directory_uri() . '/assets/js/imagesloaded.min.js', array(), null, false);
+		wp_enqueue_script('isotope', get_template_directory_uri() . '/assets/js/isotope.min.js', array('imagesloaded'), null, false);
 	}
-	wp_enqueue_script( 'custom', get_template_directory_uri() . '/assets/js/custom.js', array(), null, false );
+	wp_enqueue_script('custom', get_template_directory_uri() . '/assets/js/custom.js', array(), null, false);
 }
-add_action( 'wp_enqueue_scripts', 'load_custom_scripts_and_styles' );
-
+add_action('wp_enqueue_scripts', 'load_custom_scripts_and_styles');
 // Load custom Admin Styles
-function load_custom_admin_styles() {
-	// Admin
-	wp_enqueue_style( 'admin', get_template_directory_uri() . '/assets/css/admin.css', array(), null, false );
+function load_custom_admin_styles(){
+	wp_enqueue_style('admin', get_template_directory_uri() . '/assets/css/admin.css', array(), null, false);
 }
-add_action( 'admin_enqueue_scripts', 'load_custom_admin_styles' );
+add_action('admin_enqueue_scripts', 'load_custom_admin_styles');
 
 /*
 * displaySVG
@@ -143,13 +138,13 @@ add_action( 'admin_enqueue_scripts', 'load_custom_admin_styles' );
 * Returns: String of HTML containing optimized, inline SVG code
 * Based on https://stackoverflow.com/a/30000684/6853842
 */
-function displaySVG( $filename = '', $additional_classes = NULL ){
-    $svg_file = file_get_contents( get_template_directory_uri() . '/assets/icons/' . $filename . '.svg' );
-    $search_string = '<svg';
-    $start_position = strpos( $svg_file, $search_string );
-    $svg_code = substr( $svg_file, $start_position );
-    $output = '<div class="svg-wrapper svg-' . $filename . ' ' . $additional_classes . '">' . $svg_code . '</div>';
-    return $output;
+function displaySVG($filename = '', $additional_classes = NULL){
+	$svg_file = file_get_contents(get_template_directory_uri() . '/assets/icons/' . $filename . '.svg');
+	$search_string = '<svg';
+	$start_position = strpos($svg_file, $search_string);
+	$svg_code = substr($svg_file, $start_position);
+	$output = '<div class="svg-wrapper svg-' . $filename . ' ' . $additional_classes . '">' . $svg_code . '</div>';
+	return $output;
 }
 
 /*
@@ -160,7 +155,7 @@ function displaySVG( $filename = '', $additional_classes = NULL ){
 */
 function urlToLabel($url){
 	// Remove common prefixes
-	$label = str_replace( ["http://", "https://", "www."], "", $url );
+	$label = str_replace(["http://", "https://", "www."], "", $url);
 	// Remove paths
 	$label = explode('/', $label, 2);
 	return $label[0];
@@ -184,7 +179,7 @@ function getPortfolioItems($category=null,$tag=null,$max_items=-1){
 	);
 	$loop = new WP_Query($args);
 	$i = 0;
-	while ($loop->have_posts()){
+	while($loop->have_posts()){
 		$loop->the_post();
 		// Format Meta
 		$the_title = get_the_title();
@@ -195,12 +190,12 @@ function getPortfolioItems($category=null,$tag=null,$max_items=-1){
 		}
 		$the_date = get_the_date();
 		$the_excerpt = get_the_excerpt();
-		$the_thumbnail = get_the_post_thumbnail( null, 'medium', ['class' => 'thumbnail-image'] );
-		$the_video = get_field( 'home_page_video' );
-		$the_external_url = get_field( 'url' );
-		$the_external_url_label = mb_strimwidth( urlToLabel( $the_external_url ), 0, 25, "..." );
+		$the_thumbnail = get_the_post_thumbnail(null, 'medium', ['class' => 'thumbnail-image']);
+		$the_video = get_field('home_page_video');
+		$the_external_url = get_field('url');
+		$the_external_url_label = mb_strimwidth(urlToLabel($the_external_url), 0, 25, "...");
 		$the_permalink = get_permalink();
-		$max_items = ( $max_items > 4 || $max_items === -1) ? 4 : $max_items;
+		$max_items = ($max_items > 4 || $max_items === -1) ? 4 : $max_items;
 		$column_class = 12 / $max_items;
 		$animation_delay = $i % $max_items;
 		?>
